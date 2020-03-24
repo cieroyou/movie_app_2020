71aec6f22317185629c0ac5b5d6e0214858cc773
SeraLee <cieroyou1@gmail.com>
2020-03-24 14:31:47 +0900

    request movieList to server using axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
 
 class App extends React.Component{
   state = {
     isLoading : true,
     movies : []
   }
+  getMovies = async () =>{
+    console.log('request Movies List to yts server');
+    const movies = await axios('https://yts-proxy.now.sh/list_movies.json');
+
+  }
   componentDidMount(){
+    console.log('component did mount')
+    console.log('start setState and isLoading will changed after 6 seconds later')
+    this.getMovies();
     setTimeout(()=>{
+    console.log('isLoading changed [true] -> [false]')
       this.setState({isLoading : false})
     },6000)
+
   }
   render(){
     const {isLoading} = this.state;
