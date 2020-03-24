cbca8b1d8c39c38c913e8c14d65418c86b7065b7
SeraLee <cieroyou1@gmail.com>
2020-03-24 14:06:25 +0900

    using props

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # React Movie App
 
 니콜라스 강의를 따라하며 만든 리액트 앱
+
+## Get Movie List API from
+yts api 는 계속 구조가 바뀜
+https://yts.mx/api/v2/list_movies.json
+to
+https://yts-proxy.now.sh/list_movies.json
+
+## axios vs fetch
--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function Food({fav}){
+  return(
+    <div>
+      I like {fav}!
+    </div>
+  )
+}
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      Hello!
+      <Food fav = "abcd"/>
+    </div>
+  );
 }
 
 export default App;
