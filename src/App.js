683b288e1bb5035ded588b2640c3b961e3322494
SeraLee <cieroyou1@gmail.com>
2020-03-24 14:11:26 +0900

    using React.Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
-function Food({fav}){
-  return(
-    <div>
-      I like {fav}!
-    </div>
-  )
-}
 
-function App() {
-  return (
+class App extends React.Component{
+  render(){
+    return(
     <div>
       Hello!
-      <Food fav = "abcd"/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
