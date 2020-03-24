6e0177f3c1bfc38cfef578823b867ce4b0212434
SeraLee <cieroyou1@gmail.com>
2020-03-24 18:26:05 +0900

    add movie genre

--- src/Movie.js
+++ src/Movie.js
@@ -7,13 +7,18 @@ import PropTypes from 'prop-types';
 //     )
 // }
 
-function Movie({id, year, title, summary, poster}){
+function Movie({id, year, title, summary, poster, genres}){
     return(
     <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
+            <ul>
+                {genres.map((genre,index)=>(
+                    <li key={index} className="genres__genre">{genre}</li>
+                    )) }
+            </ul>
             <p className="movie__summary">{summary}</p>
         </div>
 
@@ -26,7 +31,8 @@ Movie.propTypes = {
     year : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
