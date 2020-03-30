2dc50c1a873ba13333393cdfe2888a6f083ada31
SeraLee <cieroyou1@gmail.com>
2020-03-30 23:55:44 +0900

    무조건 해당 무비컴포넌트를 입력해야지만 detail 페이지로 이동하도록 한다. 직접 url을 브라우저에서 입력하여 요청한 경우에는 home으로 리다이렉트 됨

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
-  return <span>This is Detail</span>;
+
+class Detail extends React.Component {
+    componentDidMount() {
+      const { location, history } = this.props;
+      //직접 url을 입력한 경우에는 location.state 가 undefined이다
+      if (location.state === undefined) {
+        //'/' route로 리다이렉트 한다
+        history.push("/");
+      }
+      console.log(location);
+    }
+    render(){
+        return <span>Movie Detail</span>
+    }
+   
 }
 export default Detail;
